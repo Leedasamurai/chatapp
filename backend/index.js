12bb97a704ebce9544fc5e73db1508503a6a15bc
Leedasamurai <lesegoleemac@gmail.com>
2024-05-23 04:18:44 +0200

    updated

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+
+  try {
+    const response = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      { headers: { "PRIVATE-KEY": "268ed6ea-d314-47e2-829e-a6ef1a924dd2" } }
+    );
+    return res.status(response.status).json(response.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+app.listen(3001, () => {
+  console.log("Server is running on port 3001");
+});
